61ba0ea1b0e44592d63ff8affb8b23ba9e6d18b2
Bahri <aribahri15@gmail.com>
2021-06-19 01:13:28 +0700

    add error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Helpers\ResponseFormatter;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +51,46 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        $response = $this->handleException($request, $exception);
+        return $response;
+    }
+
+    public function handleException($request, Throwable $exception)
+    {
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return ResponseFormatter::error(
+                null,
+                'The specified method for the request is invalid',
+                405 
+            );
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return ResponseFormatter::error(
+                null,
+                'The specified URL cannot be found',
+                404 
+            );
+        }
+
+        if ($exception instanceof HttpException) {
+            return ResponseFormatter::error(
+                null,
+                $exception->getMessage(),
+                $exception->getStatusCode() 
+            );
+        }
+
+        if (config('app.debug')) {
+            return parent::render($request, $exception);            
+        }
+
+        return ResponseFormatter::error(
+            null,
+            'Unexpected Exception. Try later',
+            500 
+        );
+
     }
 }
